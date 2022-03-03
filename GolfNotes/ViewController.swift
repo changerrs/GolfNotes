b5df2fe8673aab90053600e3425c449aad2a2336
John <jjchang1223@gmail.com>
2022-03-02 23:58:51 -0800

    added some firebase stuff

--- GolfNotes/ViewController.swift
+++ GolfNotes/ViewController.swift
@@ -6,12 +6,21 @@
 //
 
 import UIKit
+import FirebaseFirestore
 
 class ViewController: UIViewController {
+    
+    let database = Firestore.firestore()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        sendTestData()
+    }
+    
+    func sendTestData(){
+        let docRef = database.document("users/user1234")
+        docRef.setData(["name":"john", "lastName":"chang"])
     }
 
 
