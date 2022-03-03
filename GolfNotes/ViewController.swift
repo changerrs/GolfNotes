1495afdda9a30117af2ea83d9e50075b08312269
John <jjchang1223@gmail.com>
2022-03-02 23:15:46 -0800

    first

---
+++ GolfNotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GolfNotes
+//
+//  Created by John Chang on 3/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
